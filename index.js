8f654ac9fb079b8a9041a4945719d418b800e614
AIA <adashik585@gmail.com>
2022-05-27 21:10:18 +0600

    product id added

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
         });
 
+        // add product id
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
